d29fca4975f6cab77f0f5f76086d2d9399d526e7
Maverik2912 <nikitakraskovskii2912@gmail.com>
2023-07-06 12:58:11 +0300

    complete hw_2 p.2

--- src/Launch/Launch.js
+++ src/Launch/Launch.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import styles from './Launch.module.css';
 const Launch = ({launch}) => {
     const {mission_name, launch_year, links: {mission_patch_small}} = launch;
 
     return (
-        <div>
+        <div className={styles.launchContainer}>
             <p><b>Mission name:</b> "{mission_name}"</p>
             <p><b>Launch year:</b> {launch_year}</p>
             <img src={mission_patch_small} alt={mission_name}/>
--- src/Launches/Launches.js
+++ src/Launches/Launches.js
@@ -3,6 +3,7 @@ import {fetchLaunches} from "../requests/fetchLaunches";
 import Launch from "../Launch/Launch";
 import ErrorComponent from "../ErrorComponent/ErrorComponent";
 import {generateRandomId} from "../utils/getRandomId";
+import styles from './Launches.module.css';
 
 const Launches = () => {
     const [launches, setLaunches] = useState([]);
@@ -17,7 +18,7 @@ const Launches = () => {
     }, []);
 
     const allLaunches = launches
-        .filter(launch => launch.launch_year !== 2020)
+        .filter(launch => launch.launch_year !== '2020')
         .map(launch => {
         return <Launch key={generateRandomId(10)} launch={launch} />
     })
@@ -26,7 +27,10 @@ const Launches = () => {
         <div>
             {error ? <ErrorComponent error={error} />
             :
-            allLaunches
+            <div className={styles.launchesContainer}>
+                <h1 className={styles.mainTitle}>SpaceX Launches</h1>
+                <div className={styles.launches}>{allLaunches}</div>
+            </div>
             }
         </div>
     );
