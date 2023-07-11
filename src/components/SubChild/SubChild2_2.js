19d9e9599b8cb55886b5cf9cfc9ca5ecb883eb23
Maverik2912 <nikitakraskovskii2912@gmail.com>
2023-07-11 14:28:53 +0300

    review hw_3.3.1

--- src/components/SubChild/SubChild2_2.js
+++ src/components/SubChild/SubChild2_2.js
@@ -1,13 +1,16 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import styles from './SubChild.module.css';
 import {AuthorContext} from "../../App";
 const SubChild2_2 = ({subtitle}) => {
-    const {name, surname} = useContext(AuthorContext);
+
+    const [author, setAuthor] = useState({
+            name: 'Mykyta', surname: 'Kraskovskyi'
+    });
 
     return (
         <div className={styles.container}>
             <h3>{subtitle}</h3>
-            <p>Author: {name} {surname}</p>
+            <p>Author: {author.name} {author.surname}</p>
         </div>
     );
 };
