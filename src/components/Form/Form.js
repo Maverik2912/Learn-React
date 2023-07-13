85bbc562df9c434d1826095e9ef557a4ee08a7e2
Maverik2912 <nikitakraskovskii2912@gmail.com>
2023-07-13 14:33:55 +0300

    complete hw_4.3

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,97 +1,106 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from './Form.module.css';
-import { useForm } from "react-hook-form"
+import {set} from "react-hook-form";
 const Form = () => {
-    const {register, handleSubmit} = useForm();
+    const idRef = useRef(null);
+    const brandRef = useRef(null);
+    const priceRef = useRef(null);
+    const yearRef = useRef(null);
 
-    const onSubmit = (data) => {
-        fetch('https://jsonplaceholder.typicode.com/users', {
-            method: 'POST',
-            body: JSON.stringify(data),
+    const getCars = () => {
+        fetch('http://owu.linkpc.net/carsAPI/v1/cars')
+            .then(response => response.json())
+            .then(cars => console.log(cars))
+    }
+
+    const deleteCar = () => {
+        const id = idRef.current.value;
+        fetch(`http://owu.linkpc.net/carsAPI/v1/cars/${id}`, {
+            method: 'DELETE'
+        })
+            .then(response => {
+                response.ok ? console.log(`Car with id ${id} has been deleted`)
+                : console.log(`Car with id ${id} has not been found`);
+            })
+            .then(() => idRef.current.value = '')
+    }
+
+    const putCar = () => {
+        const id = idRef.current.value;
+        fetch(`http://owu.linkpc.net/carsAPI/v1/cars/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                price: priceRef.current.value,
+                brand: brandRef.current.value,
+                year: yearRef.current.value
+            }),
             headers: {
-                'Content-Type': 'application/json; charset=UTF-8'
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        })
+            .then(response => {
+                response.ok ? console.log(`Car with id ${id} has been updated`) :
+                console.log(`Car with id ${id} has not been found`);
+                brandRef.current.value = '';
+                priceRef.current.value = '';
+                idRef.current.value = '';
+                yearRef.current.value = '';
+            })
+    }
+
+    const postCar = (e) => {
+        e.preventDefault();
+        fetch(`http://owu.linkpc.net/carsAPI/v1/cars`, {
+            method: 'POST',
+            body: JSON.stringify({
+                brand: brandRef.current.value,
+                price: priceRef.current.value,
+                year: yearRef.current.value
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
             }
         })
             .then(response => response.json())
-            .then(user => console.log(user));
+            .then(data => {
+                console.log(data)
+                brandRef.current.value = '';
+                priceRef.current.value = '';
+                idRef.current.value = '';
+                yearRef.current.value = '';
+            })
     }
 
     return (
         <div className={styles.wrapper}>
-            <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-                <h1>Create user</h1>
+            <form onSubmit={postCar} className={styles.form}>
+                <h1>Cars</h1>
+
                 <p>
-                    <label htmlFor='name'>Name:</label>
-                    <input {...register('name')} id='name' type="text"/>
+                    <label htmlFor='id'>Id:</label>
+                    <input ref={idRef} id='id' type="number" />
                 </p>
 
                 <p>
-                    <label htmlFor='username'>Username:</label>
-                    <input {...register('username')} id='username' type="text"/>
+                    <label htmlFor='price'>Price:</label>
+                    <input ref={priceRef} id='price' type="number"/>
                 </p>
 
                 <p>
-                    <label htmlFor='email'>Email:</label>
-                    <input {...register('email')} id='email' type="email"/>
+                    <label htmlFor='brand'>Brand:</label>
+                    <input ref={brandRef} id='brand' type="text"/>
                 </p>
 
                 <p>
-                    <label htmlFor='city'>City:</label>
-                    <input {...register('address.city')} id='city' type="text"/>
+                    <label htmlFor='year'>Year:</label>
+                    <input ref={yearRef} id='year' type="number"/>
                 </p>
 
-                <p>
-                    <label htmlFor='suite'>Suite:</label>
-                    <input {...register('address.suite')} id='suite' type="text"/>
-                </p>
-
-                <p>
-                    <label htmlFor='street'>Street:</label>
-                    <input {...register('address.street')} id='street' type="text"/>
-                </p>
-
-                <p>
-                    <label htmlFor='zip'>Zipcode:</label>
-                    <input {...register('address.zipcode')} id='zip' type="number"/>
-                </p>
-
-                <p>
-                    <label htmlFor='lat'>Lat:</label>
-                    <input {...register('address.geo.lat')} id='lat' type="number"/>
-                </p>
-
-                <p>
-                    <label htmlFor='lng'>Lng:</label>
-                    <input {...register('address.geo.lng')} id='lng' type="number"/>
-                </p>
-
-                <p>
-                    <label htmlFor='phone'>Phone:</label>
-                    <input {...register('phone')} id='phone' type="phone"/>
-                </p>
-
-                <p>
-                    <label htmlFor='website'>Website:</label>
-                    <input {...register('website')} id='website' type="text"/>
-                </p>
-
-                <p>
-                    <label htmlFor='companyName'>Company Name:</label>
-                    <input {...register('company.name')} id='companyName' type="text"/>
-                </p>
-
-                <p>
-                    <label htmlFor='phrase'>Catch Phrase:</label>
-                    <input {...register('company.catchPhrase')} id='phrase' type="text"/>
-                </p>
-
-                <p>
-                    <label htmlFor='bs'>Bs:</label>
-                    <input {...register('company.bs')} id='bs' type="text"/>
-                </p>
-
-                <p>
-                    <button type='submit'>Send form</button>
+                <p className={styles.buttons}>
+                    <button type='button' className={styles.green} onClick={getCars}>Get cars</button>
+                    <button type='submit'>Post car</button>
+                    <button type='button' onClick={putCar} className={styles.blue}>Put car</button>
+                    <button type='button' onClick={deleteCar} className={styles.red}>Delete car</button>
                 </p>
             </form>
         </div>
