dc8da57f578a6825d12cbc6b899b7cbc14519008
Maverik2912 <nikitakraskovskii2912@gmail.com>
2023-07-20 12:20:46 +0300

    complete hw_6.1

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './ErrorPage.module.css';
+import Navbar from "../../components/Navbar/Navbar";
 const ErrorPage = () => {
     return (
-        <div className={styles.container}>
-            <h1>Error 404. Page has not been found</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className={styles.container}>
+                <h1>Error 404. Page has not been found</h1>
+            </div>
+        </>
     );
 };
 
